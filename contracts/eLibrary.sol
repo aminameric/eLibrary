756972335a9ec135ab08ee1479de22d35ad2e5cb
Amina Merić <116023819+aminameric@users.noreply.github.com>
2024-01-02 18:44:02 +0100

    struct user

---
+++ contracts/eLibrary.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.18;
+
+contract eLibrary {
+
+    struct User {
+        address userId; // ovo cu koristiti za mapiranj usera
+        string name;
+        string surname;
+        uint phoneNumber;
+        string email;
+    }
+
+
+
+}
