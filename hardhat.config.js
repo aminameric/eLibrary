39d850c03ec067bed082082dcbf069b95f83dfe4
Amina Merić <116023819+aminameric@users.noreply.github.com>
2024-01-10 16:58:04 +0100

    deployment

--- hardhat.config.js
+++ hardhat.config.js
@@ -5,7 +5,7 @@ require("dotenv").config();
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: {
-    version: "0.8.19",
+    version: "0.8.18",
     settings: {
       optimizer: {
         enabled: true,
@@ -13,4 +13,13 @@ module.exports = {
       },
     },
   }, 
+  networks: {
+    sepolia: {
+      url: `https://eth-sepolia.g.alchemy.com/v2/${process.env.API_KEY}`,
+      accounts: [process.env.SECRET_KEY],
+    },
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API,
+  },
 };
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -6,28 +6,19 @@
 // global scope, and execute the script.
 const hre = require("hardhat");
 
+
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
+ const joint = await hre.ethers.deployContract("eLibrary");
+ await joint.waitForDeployment();
 
-  const lockedAmount = hre.ethers.parseEther("0.001");
 
-  const lock = await hre.ethers.deployContract("Lock", [unlockTime], {
-    value: lockedAmount,
-  });
-
-  await lock.waitForDeployment();
-
-  console.log(
-    `Lock with ${ethers.formatEther(
-      lockedAmount
-    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.target}`
-  );
+ console.log(`Joint deployed at: ${joint.target}`);
 }
+
 
 // We recommend this pattern to be able to use async/await everywhere
 // and properly handle errors.
 main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
+ console.error(error);
+ process.exitCode = 1;
 });
